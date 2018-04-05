c3976101402049c34b8acf884ed837480f6867e3
delaRocaFrank <delarocafrank@gmail.com>
2018-04-05 15:44:37 -0600

    lap

--- inicio.php
+++ inicio.php
@@ -26,7 +26,40 @@ if(empty($_SESSION['username'])){
 <body> 
 
     <div>
+    <table>
+    <tr>
+    <th> Placa </th>
+    <th> ID </th>
+    <th> Fecha </th>
+    <th> Lugar </th>
+    <th> Monto </th>
+    <th> Descuento </th>
+    <th> Total </th>
+    <th> Motivo </th>
+    </tr>    
+    <?php
+    $datos = array(0,1,2,3,4,5,6,7,8,9,10,11,12,13,0,1,2,3,4,5,6,7,8,9,10,11,12,13);
     
+    
+    for($a=0;$a<30;$a++){
+    $flag=0;
+    foreach($datos as $dato){
+        if($flag==0){
+            echo "<tr>";
+            echo "<td> </td>";
+        }
+        echo "<td> ".$dato."-".$flag."</td>";
+        $flag+=1;
+        if($flag==7){
+            echo "</tr>";
+            $flag=0;
+        }
+
+    }
+}
+    ?>
+        
+    </table>
     </div>
 </body>
 </html>
